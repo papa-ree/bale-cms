4c95ffa687e73d53a1a9934660d7c6ffe40736e1
Papa Ree <79291092+papa-ree@users.noreply.github.com>
2025-06-20 09:29:32 +0700

    fix bug

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,30 +15,6 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        $developer = User::create([
-            'uuid' => Uuid::uuid7(),
-            'name' => 'dev',
-            'username' => 'dev',
-            'email' => 'dev@balecms.com',
-            'email_verified_at' => date("Y-m-d H:i:s"),
-            'password' => '$2y$10$WKJU.3K5/8nMxwrTZKnKa.tO9IfGeX.pNJrkMBdNzoq9DCCJ2fI6e',
-            'created_at' => date("Y-m-d H:i:s"),
-            'updated_at' => date("Y-m-d H:i:s")
-        ]);
-
-        $developer->assignRole('developer');
-
-        $admin = User::create([
-            'uuid' => Uuid::uuid7(),
-            'name' => 'bale admin',
-            'username' => 'baleadmin',
-            'email' => 'admin@balecms.com',
-            'email_verified_at' => date("Y-m-d H:i:s"),
-            'password' => '$2y$12$Bd05IQ9pSwmun7sYzaconeGl6phm6VtnKt3nYnW127JS7ct84gMOa',
-            'created_at' => date("Y-m-d H:i:s"),
-            'updated_at' => date("Y-m-d H:i:s")
-        ]);
-
-        $admin->assignRole('admin');
+        
     }
 }
